7390a30b4adeedebcb5d19a8cf77f3f0ae523ab1
yangkai <vikinn@qq.com>
2023-05-05 16:07:48 +0800

    init

---
+++ simplest-wpf-usercontrol-binding-sample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    class Model : INotifyPropertyChanged
+    {
+        private string mValue = "78";
+
+        public string MyValue
+        {
+            get { return mValue; }
+            set
+            {
+                mValue = value;
+                OnPropertyChanged(nameof(MyValue));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // LOOK THIS
+            this.DataContext = new Model();
+        }
+    }
+}
---
+++ simplest-wpf-usercontrol-binding-sample/MyUserControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserControlExample;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Value
+        {
+            get
+            {
+                return (string)GetValue(ValueProperty);
+            }
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(string),
+              typeof(MyUserControl), new PropertyMetadata(null));
+    }
+
+
+}
